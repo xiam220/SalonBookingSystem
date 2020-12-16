baede3227b401f5cac6f255ca5753e0bd7c6e85f
xiam220 <mxx70@miami.edu>
2020-12-16 01:35:51 -0500

    Entity classes and repos

---
+++ salon-api/salon-api/src/main/Slot.java
@@ -0,0 +1,42 @@
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.*;
+
+import javax.persistence.*;
+
+// import com.manning.salonapp.salonservice.SalonServiceDetail;
+import com.example.salonapi.SalonServiceDetail;
+// import jdk.jfr.events.CertificateId;
+
+import java.time.LocalDateTime;
+import java.util.Set;
+
+@Entity
+@Data
+@NoArgsConstructor
+@ToString
+public class Slot {
+
+    @Id 
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JsonIgnore
+    Set<SalonServiceDetail> availableServices;
+
+    @ManyToOne
+    private SalonServiceDetail selectedService;
+
+    String stylistName;
+
+    LocalDateTime slotFor;
+
+    private SlotStatus status;
+
+    LocalDateTime lockedAt;
+    LocalDateTime confirmedAt;
+}
+
+enum SlotStatus {
+    AVAILABLE, LOCKED, CONFIRMED, CANCELLED
+}
---
+++ salon-api/salon-api/src/main/SlotRepository.java
@@ -0,0 +1,8 @@
+import org.springframework.data.repository.CrudRepository;
+
+// import Slot;  
+
+public interface SlotRepository
+            extends CrudRepository<Slot, Long> {
+
+            }
