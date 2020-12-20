d3f29d3ca9c0c9e8621b7d86a409caf4300b8fac
xiam220 <mxx70@miami.edu>
2020-12-20 09:29:11 -0500

    Extend CRUD returns Iterable, tried to turn return type to List

--- salon-api/salon-api/src/main/java/com/example/salonapi/salonservice/SalonServiceDetailRepository.java
+++ salon-api/salon-api/src/main/java/com/example/salonapi/salonservice/SalonServiceDetailRepository.java
@@ -5,5 +5,6 @@ import org.springframework.data.repository.CrudRepository;
 // extends CrudRepository<[type of repository to persist], [entity ID property]>
 public interface SalonServiceDetailRepository          
             extends CrudRepository<SalonServiceDetail, Long> {
-
+    // @Override
+    // List<SalonServiceDetail> findAll();
             }
