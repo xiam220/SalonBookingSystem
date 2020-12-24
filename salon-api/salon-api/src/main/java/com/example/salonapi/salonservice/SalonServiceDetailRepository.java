c28aa7b9d33e87008ba215d8c49b56c69298bd8e
xiam220 <mxx70@miami.edu>
2020-12-24 14:20:06 -0500

    Cast Iterable to List

--- salon-api/salon-api/src/main/java/com/example/salonapi/salonservice/SalonServiceDetailRepository.java
+++ salon-api/salon-api/src/main/java/com/example/salonapi/salonservice/SalonServiceDetailRepository.java
@@ -1,10 +1,12 @@
 package com.example.salonapi.salonservice;
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 
 // extends CrudRepository<[type of repository to persist], [entity ID property]>
 public interface SalonServiceDetailRepository          
             extends CrudRepository<SalonServiceDetail, Long> {
-    // @Override
-    // List<SalonServiceDetail> findAll();
-            }
+    @Override
+    List<SalonServiceDetail> findAll();
+}
